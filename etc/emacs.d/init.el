24601c86a27f597d89c416301ccc53eb65265628
K. Adam Christensen <pope@shifteleven.com>
2011-05-13 10:36:56 -0700

    If el-get isn't installed, it bootstraps it

--- etc/emacs.d/init.el
+++ etc/emacs.d/init.el
@@ -64,7 +64,13 @@
 ;;
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(require 'el-get)
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
 (require 'inversion) ;; needed by cedet
 
 (setq package-user-dir "~/.emacs.d/elpa")
