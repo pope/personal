9694504d3075b09db546ace709156a429227cdf9
K. Adam Christensen <pope@shifteleven.com>
2012-05-14 21:44:27 -0700

    More monokai theme updates.

--- etc/emacs.d/monokai-theme.el
+++ etc/emacs.d/monokai-theme.el
@@ -47,17 +47,18 @@
    ;; Frame
    `(default ((t (:foreground ,mt-fg :background ,mt-bg))))
    `(cursor ((t (:foreground ,mt-fg))))
-   `(highlight ((t (:background ,mt-hi))))
+   `(highlight ((t (:background ,mt-sel-border))))
    `(minibuffer-prompt ((t (:foreground ,mt-red))))
-
    `(mode-line ((t (:background ,mt-bg :foreground ,mt-white))))
    `(mode-line-inactive ((t (:background ,mt-hi :foreground ,mt-fg))))
-   `(region ((t (:background ,mt-sel))))
+   `(region ((t (:background ,mt-hi))))
    `(fringe ((t (:background ,mt-bg :foreground ,mt-invis))))
    `(vertical-border ((t (:foreground ,mt-black))))
 
    ;; Main
+   '(variable-pitch ((t (:family "Arial"))))
    `(show-paren-match-face ((t (:foreground ,mt-fg :underline t :background ,mt-hi))))
+   `(escape-glyph ((t (:foreground ,mt-invis))))
    `(font-lock-builtin-face ((t (:foreground ,mt-blue))))
    `(font-lock-comment-face ((t (:foreground ,mt-mud))))
    `(font-lock-constant-face ((t (:foreground ,mt-purple))))
@@ -70,6 +71,11 @@
    `(font-lock-variable-name-face ((t (:foreground ,mt-blue))))
    `(font-lock-warning-face ((t (:bold t :foreground ,mt-red))))
 
+   ;; isearch
+   `(isearch ((t (:background ,mt-yellow :foreground ,mt-bg))))
+   `(isearch-fail ((t (:background ,mt-orange :foreground ,mt-bg))))
+   `(lazy-highlight ((t (:background ,mt-sel :foreground nil))))
+
    ;; Minimap
    `(minimap-active-region-background ((t (:background ,mt-hi))))
 
@@ -78,6 +84,43 @@
    `(js2-jsdoc-type ((t (:italic t :foreground ,mt-mud))))
    `(js2-jsdoc-value ((t (:bold t :italic t :foreground ,mt-mud))))
    `(js2-function-param ((t (:foreground ,mt-orange))))
+
+   ;; Dired
+   `(dired-directory ((t (:foreground ,mt-purple))))
+   `(dired-symlink ((t (:foreground ,mt-blue))))
+   `(dired-mark ((t (:foreground ,mt-green))))
+   `(dired-flagged ((t (:foreground ,mt-orange))))
+   `(dired-marked ((t (:foreground ,mt-yellow))))
+   `(dired-header ((t (:foreground ,mt-fg :height 1.4 :underline t :inherit 'variable-pitch))))
+
+   ;; Org
+   `(org-done ((t (:foreground ,mt-green))))
+   `(org-todo ((t (:foreground ,mt-red))))
+   `(org-document-info ((t (:foreground ,mt-yellow))))
+   `(org-document-title ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 2.0))))
+   `(outline-1 ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 1.8))))
+   `(outline-2 ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 1.6))))
+   `(outline-3 ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 1.4))))
+   `(outline-4 ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 1.2))))
+   `(outline-5 ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 1.1))))
+   `(outline-6 ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 1.1))))
+   `(outline-7 ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 1.1))))
+   `(outline-8 ((t (:foreground ,mt-fg :inherit 'variable-pitch :slant italic :height 1.1))))
+
+   ;; Magit
+   `(magit-diff-add ((t (:foreground ,mt-green))))
+   `(magit-diff-del ((t (:foreground ,mt-red))))
+   `(magit-log-sha1 ((t (:foreground ,mt-orange))))
+
+   ;; UI
+   `(link ((t (:foreground ,mt-blue))))
+   `(link-visited ((t (:foreground ,mt-purple))))
+   `(button ((t (:foreground ,mt-blue))))
+
+   ;; Ido
+   `(ido-subdir ((t (:foreground ,mt-purple))))
+   `(ido-only-match ((t (:foreground ,mt-green))))
+   `(ido-virtual ((t (:foreground ,mt-blue))))
 
    ;; Whitespace
    `(whitespace-tab ((t (:foreground ,mt-invis :background nil))))
