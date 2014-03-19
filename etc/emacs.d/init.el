aa5bb1832a5c1a409d312db954aca4213dbadb36
Adam Christensen <pope@google.com>
2014-03-19 10:04:06 -0700

    Add revert-all-buffers function.

--- etc/emacs.d/init.el
+++ etc/emacs.d/init.el
@@ -174,6 +174,16 @@
   (shell-command-on-region (point-min) (point-max) "python -mjson.tool" t)
   (js-mode))
 
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name)
+                 (file-exists-p (buffer-file-name))
+                 (not (buffer-modified-p)))
+        (revert-buffer t t t))))
+  (message "Refreshed open files."))
 
 
 ;;
