8369d2c72dcdbc49ceb4f686769fb037275fae87
K. Adam Christensen <pope@shifteleven.com>
2015-04-11 13:49:56 -0700

    More i3 startup stuff.

--- etc/i3/bin/startup.sh
+++ etc/i3/bin/startup.sh
@@ -17,6 +17,18 @@ if command -v gnome-settings-daemon > /dev/null 2>&1; then
   gnome-settings-daemon &
 fi
 
+if command -v gnome-keyring-daemon > /dev/null 2>&1; then
+  gnome-keyring-daemon
+fi
+
+if [[ -f /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 ]]; then
+  /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+fi
+
+if command -v nm-applet > /dev/null 2>&1; then
+  nm-applet &
+fi
+
 if [[ -f ~/.fehbg ]] ; then
   sh ~/.fehbg
 fi
