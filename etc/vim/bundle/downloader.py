84bc68975d963de9621f6ff128f98c83de3e78be
K. Adam Christensen <pope@shifteleven.com>
2020-06-08 08:36:56 -0700

    Add editorconfig-vim.

--- etc/vim/bundle/downloader.py
+++ etc/vim/bundle/downloader.py
@@ -10,12 +10,13 @@ import download
 logging.basicConfig(
     format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
 
-dm = download.DownloadManager(4, force=True)
+dm = download.DownloadManager(4, force=False)
 dm.github_download_repo('MatchTagAlways', 'Valloric', 'master')
 dm.github_download_repo('NrrwRgn', 'chrisbra', 'master')
 dm.github_download_repo('base16-vim', 'chriskempson', 'master')
 dm.github_download_repo('bufexplorer', 'jlanzarotta', 'v7.4.21')
 dm.github_download_repo('ctrlp.vim', 'ctrlpvim', 'master')
+dm.github_download_repo('editorconfig', 'editorconfig-vim', 'v1.1.1')
 dm.github_download_repo('molokai', 'tomasr', 'master')
 dm.github_download_repo('nerdtree', 'preservim', '6.7.15')
 dm.github_download_repo('slimux', 'epeli', 'master')
