3732bb818dd1fc110ad424f154eed15ac5cac016
K. Adam Christensen <pope@shifteleven.com>
2022-12-21 17:34:15 -0800

    Bring back the netw thingie.

--- etc/nvim/lua/pope/options.lua
+++ etc/nvim/lua/pope/options.lua
@@ -1,7 +1,3 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
