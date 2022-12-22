48e1bbd2e4a0fd5ade7a5ad87bfbc407a008238c
K. Adam Christensen <pope@shifteleven.com>
2022-12-21 22:21:56 -0800

    Spiff up bufferline and nvim-tree.

--- etc/nvim/after/plugin/nvim-tree.lua
+++ etc/nvim/after/plugin/nvim-tree.lua
@@ -1,2 +1,12 @@
-require("nvim-tree").setup()
-
+require("nvim-tree").setup({
+  open_on_setup = true,
+  renderer = {
+    indent_markers = {
+      enable = true,
+    }
+  },
+  sync_root_with_cwd = true,
+  update_focused_file = {
+    enable = true,
+  },
+})
