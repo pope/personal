9d3c5e268947b4093b067566fedc96999c873ed8
K. Adam Christensen <pope@shifteleven.com>
2022-12-23 15:18:48 -0800

    Using base variant of rose-pine.

--- etc/nvim/after/plugin/colors.lua
+++ etc/nvim/after/plugin/colors.lua
@@ -1,19 +1,19 @@
 require('rose-pine').setup({
-  dark_variant = 'moon',
-  disable_background = true
+	dark_variant = 'main',
+	disable_background = true
 })
 
 function ColorMyPencils(color)
-  color = color or 'rose-pine'
-  vim.cmd.colorscheme(color)
+	color = color or 'rose-pine'
+	vim.cmd.colorscheme(color)
 
-  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
-  if color == 'rose-pine' then
-    vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#2a283e' })
-    vim.api.nvim_set_hl(0, 'NonText', { fg = '#44415a', bg = 'none' })
-  end
+	if color == 'rose-pine' then
+		vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#21202e' })
+		vim.api.nvim_set_hl(0, 'NonText', { fg = '#403d52', bg = 'none' })
+	end
 end
 
 --ColorMyPencils('dracula')
