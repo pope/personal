6d3d381a083c173618262f073ff253e706454967
K. Adam Christensen <pope@shifteleven.com>
2022-12-23 15:09:33 -0800

    Add fileformatting on save.

--- etc/nvim/after/plugin/lsp.lua
+++ etc/nvim/after/plugin/lsp.lua
@@ -1,11 +1,16 @@
-local lsp = require('lsp-zero')
+local status_ok, lsp = pcall(require, 'lsp-zero')
+if not status_ok then
+	return
+end
 
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-  'sumneko_lua'
+	'sumneko_lua'
 })
 
 lsp.nvim_workspace()
 
 lsp.setup()
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
