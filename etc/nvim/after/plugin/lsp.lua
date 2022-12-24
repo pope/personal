4c0c2d133303518651d7ec538f23315973e4717f
K. Adam Christensen <pope@shifteleven.com>
2022-12-24 11:13:19 -0800

    Making sure LSP is configured for code folding.

--- etc/nvim/after/plugin/lsp.lua
+++ etc/nvim/after/plugin/lsp.lua
@@ -10,6 +10,15 @@ lsp.ensure_installed({
 	'sumneko_lua'
 })
 
+-- code folding
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
+}
+require('cmp_nvim_lsp').default_capabilities(capabilities)
+
 lsp.nvim_workspace()
 
 lsp.setup()
