6a5f7d14624859f6845e918c519c15fab7e2828f
K. Adam Christensen <pope@shifteleven.com>
2023-01-06 17:53:02 -0800

    Moved fugitive and undotree mappings to remap file.

--- etc/nvim/lua/pope/remap.lua
+++ etc/nvim/lua/pope/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.NvimTreeToggle)
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
