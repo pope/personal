d692c0c37fc2550f325f103005b1fd0064719f9e
K. Adam Christensen <pope@shifteleven.com>
2023-02-20 14:07:57 -0800

    Add protobuf support.

--- etc/nvim/lua/pope/packer.lua
+++ etc/nvim/lua/pope/packer.lua
@@ -121,6 +121,9 @@ return require('packer').startup(function(use)
 
 	use 'akinsho/toggleterm.nvim'
 
+	-- Languages
+	use 'wfxr/protobuf.vim'
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
