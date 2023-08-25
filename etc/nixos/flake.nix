80bdb6c323caec1c64932b3e85bf98ac14cb4a9b
K. Adam Christensen <pope@shifteleven.com>
2023-08-25 09:11:03 -0700

    Use unstable home-manager with unstable nixos. Also do not include hyprland in soundwave config

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -20,7 +20,9 @@
     nixpkgs-stable.url = "github:NixOS/nixpkgs/release-23.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      # The stable URL
+      # url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
