1e1f070fc7a42de05a7ab2cc3c83ee31f146dbe4
K. Adam Christensen <pope@shifteleven.com>
2023-09-24 14:42:22 -0700

    Update some configs for Hyprland

--- etc/nixos/modules/hyprland.nix
+++ etc/nixos/modules/hyprland.nix
@@ -1,10 +1,17 @@
-{ hyprland, pkgs, ... }:
+{ inputs, pkgs, ... }:
 
+let
+  overlays = import ../overlays;
+in
 {
+  nixpkgs.overlays = with overlays; [
+    waybar
+  ];
+
   services = {
     dbus = {
       enable = true;
-      packages = [ pkgs.gcr ];
+      packages = with pkgs; [ gcr dconf ];
     };
 
     geoclue2.enable = true;
@@ -35,12 +42,12 @@
       };
 
       displayManager = {
-        defaultSession = "hyprland";
-        lightdm.enable = false;
-        gdm = {
-          enable = true;
-          wayland = true;
-        };
+        # defaultSession = "hyprland";
+        lightdm.enable = true;
+        # gdm = {
+        #   enable = true;
+        #   wayland = true;
+        # };
       };
     };
   };
@@ -50,18 +57,27 @@
     polkit.enable = true;
   };
 
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   xdg = {
-    # autostart.enable = true;
     portal = {
       enable = true;
       wlr.enable = true;
-      # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1
-      # This will make xdg-open use the portal to open programs,
-      # which resolves bugs involving programs opening inside FHS envs or with unexpected env vars set from wrappers.
-      # xdg-open is used by almost all programs to open a unknown file/uri
-      # alacritty as an example, it use xdg-open as default, but you can also custom this behavior
-      # and vscode has open like `External Uri Openers`
-      xdgOpenUsePortal = false;
+
       extraPortals = with pkgs; [
         xdg-desktop-portal-wlr # for wlroots based compositors(hyprland/sway)
         xdg-desktop-portal-gtk # for gtk
@@ -74,9 +90,8 @@
 
     hyprland = {
       enable = true;
-      package = hyprland.packages.${pkgs.system}.hyprland;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       xwayland.enable = true;
-      enableNvidiaPatches = true;
     };
 
     light.enable = true;
@@ -93,35 +108,11 @@
   environment = {
     pathsToLink = [ "/libexec" ];
 
-    sessionVariables = {
-      # If your cursor becomes invisible
-      WLR_NO_HARDWARE_CURSORS = "1";
-      # Hint electron apps to use wayland
-      NIXOS_OZONE_WL = "1";
-
-      # https://www.reddit.com/r/NixOS/comments/137j18j/need_guide_on_installing_hyprland/
-      # CLUTTER_BACKEND = "wayland";
-      # GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
-      # GTK_USE_PORTAL = "1";
-      # NIXOS_XDG_OPEN_USE_PORTAL = "1";
-      # POLKIT_AUTH_AGENT = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-      # XDG_CURRENT_DESKTOP = "Hyprland";
-      # XDG_SESSION_DESKTOP = "Hyprland";
-
-      # WLR_RENDERER = "vulkan";
-      # _JAVA_AWT_WM_NONREPARENTING = "1";
-      # MOZ_ENABLE_WAYLAND = "1";
-
-      POPE_XDP = "${pkgs.xdg-desktop-portal}";
-      POPE_XDP_GTK = "${pkgs.xdg-desktop-portal-gtk}";
-      POPE_XDP_HYPR = "${pkgs.xdg-desktop-portal-hyprland}";
-    };
-
     systemPackages = with pkgs; [
       # xdg-utils
-      # xdg-desktop-portal
-      # xdg-desktop-portal-gtk
-      # xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+
       alsa-utils
       dunst
       gnome.adwaita-icon-theme
@@ -152,10 +143,6 @@
       wlr-randr
       xfce.thunar
       waybar
-      (waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-      })
-      )
     ];
   };
 }
