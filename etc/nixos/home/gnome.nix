35e748885271323c34e893d2cd3b9487a19341bc
K. Adam Christensen <pope@shifteleven.com>
2023-09-24 21:53:59 -0700

    Gnome settings.

--- etc/nixos/home/gnome.nix
+++ etc/nixos/home/gnome.nix
@@ -2,9 +2,6 @@
 
 {
   home.packages = with pkgs; [
-    # TODO(pope): Move this to maybe a browsers file.
-    firefox
-
     gnomeExtensions.app-icons-taskbar
     gnomeExtensions.appindicator
     gnomeExtensions.blur-my-shell
@@ -12,5 +9,25 @@
     gnomeExtensions.forge
     gnomeExtensions.pop-shell
     gnomeExtensions.rounded-window-corners
+    gnomeExtensions.user-themes
   ];
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+
+      # `gnome-extensions list` for a list
+      enabled-extensions = with pkgs.gnomeExtensions; [
+        appindicator.extensionUuid
+        user-themes.extensionUuid
+        caffeine.extensionUuid
+      ];
+    };
+
+    "org/gnome/shell/extensions/user-theme".name = "rose-pine-moon";
+  };
 }
