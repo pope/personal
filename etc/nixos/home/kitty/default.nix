e898c4de4d0ec73fe13d4b052257e0ca446803bd
K. Adam Christensen <pope@shifteleven.com>
2023-09-29 14:59:22 -0700

    Update kitty config.

--- etc/nixos/home/kitty/default.nix
+++ etc/nixos/home/kitty/default.nix
@@ -8,10 +8,15 @@
       size = 11.0;
     };
     settings = {
-      adjust_line_height = "125%";
       background_opacity = "0.85";
       clipboard_control = "write-clipboard write-primary read-clipboard-ask read-primary-ask";
+      dynamic_background_opacity = "yes";
       shell = "${pkgs.fish}/bin/fish --login";
+      tab_bar_edge = "top";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "round";
+
+      "modify_font cell_height" = "125%";
     };
     # theme = "Catppuccin-Mocha";
     theme = "Rosé Pine";
