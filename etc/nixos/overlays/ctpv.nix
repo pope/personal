ecbba961530cd746c951e7d689f4273c85be2200
K. Adam Christensen <pope@shifteleven.com>
2023-09-30 11:09:16 -0700

    Add icons in lf.

--- etc/nixos/overlays/ctpv.nix
+++ etc/nixos/overlays/ctpv.nix
@@ -1,7 +1,7 @@
 _final: prev:
 {
   ctpv = (prev.ctpv.override {
-    stdenv = prev.llvmPackages_16.stdenv;
+    inherit (prev.llvmPackages_16) stdenv;
   }).overrideAttrs (oldAttrs: {
     meta.platforms = oldAttrs.meta.platforms ++ [ "aarch64-darwin" ];
   });
