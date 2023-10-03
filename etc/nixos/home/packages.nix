d629689e346a661f058ccd709749c364223a34e5
K. Adam Christensen <pope@shifteleven.com>
2023-10-02 21:39:56 -0700

    Add fzf colors.

--- etc/nixos/home/packages.nix
+++ etc/nixos/home/packages.nix
@@ -3,7 +3,6 @@
 {
   home.packages = with pkgs; [
     fd
-    fzf
     htop
     imagemagick
     neofetch
@@ -42,6 +41,27 @@
       plugins = [
         { name = "tide"; inherit (pkgs.fishPlugins.tide) src; }
       ];
+    };
+
+    fzf = {
+      enable = true;
+      colors = {
+        "bg+" = "#26233a";
+        "fg+" = "#e0def4";
+        "hl+" = "#ebbcba";
+        bg = "#191724";
+        border = "#403d52";
+        fg = "#908caa";
+        gutter = "#191724";
+        header = "#31748f";
+        hl = "#ebbcba";
+        info = "#9ccfd8";
+        marker = "#eb6f92";
+        pointer = "#c4a7e7";
+        prompt = "#908caa";
+        separator = "#403d52";
+        spinner = "#f6c177";
+      };
     };
   };
 
