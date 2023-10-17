2529cd31f64eed2cb11b89d99259d711f1027e60
K. Adam Christensen <pope@shifteleven.com>
2023-10-17 14:06:17 -0700

    Fix typo.

--- etc/nixos/home/hyprland/default.nix
+++ etc/nixos/home/hyprland/default.nix
@@ -9,7 +9,7 @@ let
           ${hyprland}/bin/hyprctl --batch "\
               keyword animations:enabled 0;\
               keyword decoration:drop_shadow 0;\
-              keyword decoratioscriptn:blur:enabled 0;\
+              keyword decoration:blur:enabled 0;\
               keyword general:gaps_in 0;\
               keyword general:gaps_out 0;\
               keyword general:border_size 1;\
