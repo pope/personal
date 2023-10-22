337b04bc8f3a0e8c511b6c0e4639bcff4622623a
K. Adam Christensen <pope@shifteleven.com>
2023-10-22 09:12:35 -0700

    Add `plow` to the flake inputs.

--- etc/nixos/overlays/plow.nix
+++ etc/nixos/overlays/plow.nix
@@ -1,12 +1,8 @@
-_: (_final: prev: {
+{ inputs, ... }: (_final: prev: {
   plow = prev.buildGoModule {
     name = "plow";
-    src = prev.fetchFromGitHub {
-      owner = "six-ddc";
-      repo = "plow";
-      rev = "343b7510ccfa477d9c0f3d9aeeaa0834e118c44a";
-      sha256 = "sha256-TynFq7e4MtZlA5SmGMybhmCVw67yHYgZWffQjuyhTDA=";
-    };
+    version = "1.3.1";
+    src = inputs.plow;
     vendorSha256 = "sha256-t2lBPyCn8bu9hLsWmaCGir9egbX0mQR+8kB0RfY7nHE=";
   };
 })
