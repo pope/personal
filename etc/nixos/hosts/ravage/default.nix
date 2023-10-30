340043b052d1ea83d0f89b3ce928e2f3ad18a5a9
K. Adam Christensen <pope@shifteleven.com>
2023-10-29 20:51:25 -0700

    Enable steam hardware on ravage

--- etc/nixos/hosts/ravage/default.nix
+++ etc/nixos/hosts/ravage/default.nix
@@ -85,14 +85,18 @@ in
     };
   };
 
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
+  hardware = {
+    opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+    };
+
+    steam-hardware.enable = true;
   };
 
   services = {
