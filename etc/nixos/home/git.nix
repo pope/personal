da9dfb9522d1b142893c8966477adf7ce971bfcd
K. Adam Christensen <pope@shifteleven.com>
2023-12-11 17:58:37 -0800

    Add fix for gh.

--- etc/nixos/home/git.nix
+++ etc/nixos/home/git.nix
@@ -23,7 +23,14 @@
       };
     };
 
-    gh.enable = true;
+    gh = {
+      enable = true;
+
+      settings = {
+        # Workaround for https://github.com/nix-community/home-manager/issues/4744
+        version = 1;
+      };
+    };
 
     ssh = {
       enable = true;
