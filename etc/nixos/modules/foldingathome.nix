ca16995cda1803f305e4cb7bea0632768fee2165
K. Adam Christensen <pope@shifteleven.com>
2024-01-07 12:40:42 -0800

    Migrate Folding@Home module to use options.

--- etc/nixos/modules/foldingathome.nix
+++ etc/nixos/modules/foldingathome.nix
@@ -1,22 +1,33 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.my.system.fah;
+in
 {
-  nixpkgs.config.permittedInsecurePackages = [
-    "python-2.7.18.7"
-    "python-2.7.18.7-env"
-  ];
-
-  services.foldingathome = {
-    enable = false;
-    user = "ShiftEleven";
-    team = 236565;
+  options.my.system.fah = {
+    enable = mkEnableOption "Folding at Home";
   };
 
-  environment.systemPackages = with pkgs; [
-    # The below doesn't work for me yet. However, I can install and run the
-    # controller with:
-    # NIXPKGS_ALLOW_UNFREE=1 NIXPKGS_ALLOW_INSECURE=1 nix-shell -p fahcontrol --run FAHControl
-    # fahcontrol
-    fahviewer
-  ];
+  config = mkIf cfg.enable {
+
+    nixpkgs.config.permittedInsecurePackages = [
+      "python-2.7.18.7"
+      "python-2.7.18.7-env"
+    ];
+
+    services.foldingathome = {
+      enable = true;
+      user = "ShiftEleven";
+      team = 236565;
+    };
+
+    environment.systemPackages = with pkgs; [
+      # The below doesn't work for me yet. However, I can install and run the
+      # controller with:
+      # NIXPKGS_ALLOW_UNFREE=1 NIXPKGS_ALLOW_INSECURE=1 nix-shell -p fahcontrol --run FAHControl
+      # fahcontrol
+      fahviewer
+    ];
+  };
 }
