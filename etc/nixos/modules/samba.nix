622f1326a1b289912ea3474d6ba295ba604d9708
K. Adam Christensen <pope@shifteleven.com>
2024-01-07 16:38:51 -0800

    Move share configuration into samba module.

--- etc/nixos/modules/samba.nix
+++ etc/nixos/modules/samba.nix
@@ -1,12 +1,23 @@
 { config, lib, ... }:
 
 let
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib) mkIf mkEnableOption mkOption types;
   cfg = config.my.system.samba;
 in
 {
   options.my.system.samba = {
     enable = mkEnableOption "samba system options";
+    shares = mkOption {
+      default = { };
+      description = "Samba shares";
+      type = with types; attrsOf (submodule {
+        options = {
+          path = mkOption {
+            type = str;
+          };
+        };
+      });
+    };
   };
 
   config = mkIf cfg.enable {
@@ -37,19 +48,42 @@ in
 
       samba-wsdd.enable = true; # make shares visible for windows 10 clients
 
-      samba = {
-        enable = true;
-        openFirewall = true;
-        securityType = "user";
-        extraConfig = ''
-          browsable = yes
-          smb encrypt = required
-          security = user 
+      samba =
+        let
+          inherit (builtins) listToAttrs map getAttr attrNames;
+          shareConfig = name:
+            let
+              share = getAttr name cfg.shares;
+            in
+            {
+              inherit name;
+              value = {
+                inherit (share) path;
+                browseable = "yes";
+                "read only" = "no";
+                "guest ok" = "no";
+                "create mask" = "0644";
+                "directory mask" = "0755";
+                "force user" = config.my.system.mainUser;
+                "force group" = "users";
+              };
+            };
+          shares = listToAttrs (map shareConfig (attrNames cfg.shares));
+        in
+        {
+          inherit shares;
+          enable = true;
+          openFirewall = true;
+          securityType = "user";
+          extraConfig = ''
+            browsable = yes
+            smb encrypt = required
+            security = user 
 
-          guest account = nobody
-          map to guest = bad user
-        '';
-      };
+            guest account = nobody
+            map to guest = bad user
+          '';
+        };
     };
   };
 }
