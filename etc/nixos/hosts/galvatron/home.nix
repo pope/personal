737ab1797b5c70f8bbc18bfd9cc5b0151dda4e4a
K. Adam Christensen <pope@shifteleven.com>
2024-01-15 11:26:50 -0800

    Configure git for galvatron.
    This also meant I needed to move some SSH config stuff to the galvatron
    home file.

--- etc/nixos/hosts/galvatron/home.nix
+++ etc/nixos/hosts/galvatron/home.nix
@@ -34,10 +34,30 @@ in
 
   programs = {
     home-manager.enable = true;
+
+    ssh = {
+      controlMaster = "auto";
+      controlPath = "/tmp/%r@%h:%p";
+      controlPersist = "5m";
+
+      matchBlocks."shifteleven.com".user = "root";
+
+      matchBlocks."nix-builder" = {
+        user = "root";
+        hostname = "127.0.0.1";
+        port = 3022;
+        identityFile = "~/.ssh/insecure_rsa";
+      };
+    };
   };
 
   my.home = {
     audio.enable = true;
+    git = {
+      enable = true;
+      opIdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      opSshSignCommand = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+    };
     # TODO(pope) : Enable nvim here.
     languages = {
       javascript.enable = true;
