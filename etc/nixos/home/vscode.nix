8bcaf3d2f9cbebe68be586cafcda64969f21c7eb
K. Adam Christensen <pope@shifteleven.com>
2024-01-15 16:41:52 -0800

    Make vscode available under my.home.vscode

--- etc/nixos/home/vscode.nix
+++ etc/nixos/home/vscode.nix
@@ -1,19 +1,29 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
+let
+  inherit (lib) mkIf mkEnableOption;
+  cfg = config.my.home.vscode;
+in
 {
-  programs.vscode = {
-    enable = false;
-    package = pkgs.vscodium.fhsWithPackages (ps: with ps; [ zlib ]);
-    extensions = with pkgs.vscode-extensions; [
-      golang.go
-      ms-python.python
-      ms-vscode.cpptools
-      mvllow.rose-pine
-      rust-lang.rust-analyzer
-      vscode-icons-team.vscode-icons
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-      zxh404.vscode-proto3
-    ];
+  options.my.home.vscode = {
+    enable = mkEnableOption "VSCode home options";
+  };
+
+  config = mkIf cfg.enable {
+    programs.vscode = {
+      enable = false;
+      package = pkgs.vscodium.fhsWithPackages (ps: with ps; [ zlib ]);
+      extensions = with pkgs.vscode-extensions; [
+        golang.go
+        ms-python.python
+        ms-vscode.cpptools
+        mvllow.rose-pine
+        rust-lang.rust-analyzer
+        vscode-icons-team.vscode-icons
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        zxh404.vscode-proto3
+      ];
+    };
   };
 }
