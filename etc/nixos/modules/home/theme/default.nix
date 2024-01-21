cbb3711acbb46060dc3e4097ee59181557734e1f
K. Adam Christensen <pope@shifteleven.com>
2024-01-20 16:12:25 -0800

    Use a better syntax for setting colors in the theme

--- etc/nixos/modules/home/theme/default.nix
+++ etc/nixos/modules/home/theme/default.nix
@@ -17,9 +17,7 @@ in
 
   config =
     let
-      colors = {
-        inherit (cfg.colorScheme.colors) base00 base01 base02 base03 base04 base05 base06 base07;
-        inherit (cfg.colorScheme.colors) base08 base09 base0A base0B base0C base0D base0E base0F;
+      colors = cfg.colorScheme.colors // {
         withHash = builtins.mapAttrs (_k: v: "#${v}") cfg.colorScheme.colors;
         withHex = builtins.mapAttrs (_k: v: "0x${v}") cfg.colorScheme.colors;
       };
