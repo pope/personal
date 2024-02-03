b92d2bfe58de6f320c85df16a3728d8967065ddb
K. Adam Christensen <pope@shifteleven.com>
2024-02-03 09:18:57 -0800

    Add support for an 8BitDo controller

--- etc/nixos/modules/nixos/gaming.nix
+++ etc/nixos/modules/nixos/gaming.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   inherit (lib) mkIf mkEnableOption;
@@ -10,6 +10,24 @@ in
   };
 
   config = mkIf cfg.enable {
+    # 8BitDo support
+
+    # Udev rules to start or stop systemd service when controller is connected or disconnected
+    services.udev.extraRules = ''
+      # May vary depending on your controller model, find product id using 'lsusb'
+      SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3106", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl start 8bitdo-ultimate-xinput@2dc8:3106"
+      # This device (2dc8:3019) is "connected" when the above device disconnects
+      SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3016", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl stop 8bitdo-ultimate-xinput@2dc8:3106"
+    '';
+    # Systemd service which starts xboxdrv in xbox360 mode
+    systemd.services."8bitdo-ultimate-xinput@" = {
+      unitConfig.Description = "8BitDo Ultimate Controller XInput mode xboxdrv daemon";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.xboxdrv}/bin/xboxdrv --mimic-xpad --silent --type xbox360 --device-by-id %I --force-feedback";
+      };
+    };
+
     hardware = {
       steam-hardware.enable = true;
     };
