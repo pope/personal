2833ca95e4063abb31ef77e28d6b053e4cc07762
K. Adam Christensen <pope@shifteleven.com>
2024-02-05 20:43:07 -0800

    Dynamically import top-level modules.
    When doing this, I found 3 home modules that I hadn't converted over to
    an options based module.

--- etc/nixos/modules/home/default.nix
+++ etc/nixos/modules/home/default.nix
@@ -1,27 +1,19 @@
 { inputs, ... }:
 
+let
+  imports = map
+    (p: ./. + "/${p}")
+    (builtins.filter
+      (p: p != "default.nix"
+        # TODO(pope): Turn these into config-based modules.
+        && p != "gaming"
+        && p != "hyprland"
+        && p != "waybar")
+      (builtins.attrNames (builtins.readDir ./.)));
+in
 {
   imports = [
     inputs.anyrun.homeManagerModules.default
     inputs.nix-colors.homeManagerModules.default
-
-    ./anyrun
-    ./audio
-    ./browsers
-    ./dropbox
-    ./dunst
-    ./editors
-    ./git
-    ./gnome
-    ./gtk
-    ./keymapp
-    ./languages
-    ./lf
-    ./mpv
-    ./packages
-    ./shell
-    ./terminals
-    ./theme
-    ./xdg
-  ];
+  ] ++ imports;
 }
