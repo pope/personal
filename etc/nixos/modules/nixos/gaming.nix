fa56d6eb25eab288ed7a5a307e7654ffe40b9afc
K. Adam Christensen <pope@shifteleven.com>
2024-02-08 21:21:32 -0800

    Fix a typo on the 8BitDo disconnect udev rule
    Also added a thank you to the Gist that set that up.

--- etc/nixos/modules/nixos/gaming.nix
+++ etc/nixos/modules/nixos/gaming.nix
@@ -11,14 +11,17 @@ in
 
   config = mkIf cfg.enable {
     # 8BitDo support
+    # Thank you https://gist.github.com/interdependence/28452fbfbe692986934fbe1e54c920d4
 
     # Udev rules to start or stop systemd service when controller is connected or disconnected
     services.udev.extraRules = ''
       # May vary depending on your controller model, find product id using 'lsusb'
       SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3106", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl start 8bitdo-ultimate-xinput@2dc8:3106"
-      # This device (2dc8:3019) is "connected" when the above device disconnects
-      SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3016", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl stop 8bitdo-ultimate-xinput@2dc8:3106"
+
+      # This device is connected when the above device disconnects
+      SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3019", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl stop 8bitdo-ultimate-xinput@2dc8:3106"
     '';
+
     # Systemd service which starts xboxdrv in xbox360 mode
     systemd.services."8bitdo-ultimate-xinput@" = {
       unitConfig.Description = "8BitDo Ultimate Controller XInput mode xboxdrv daemon";
@@ -28,6 +31,7 @@ in
       };
     };
 
+    # Hardware support from Steam
     hardware = {
       steam-hardware.enable = true;
     };
