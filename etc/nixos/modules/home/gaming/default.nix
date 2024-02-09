2d18f2647cb2bc60230108c03b34c5432b857151
K. Adam Christensen <pope@shifteleven.com>
2024-02-08 21:09:39 -0800

    Make gaming a option-based module.

--- etc/nixos/modules/home/gaming/default.nix
+++ etc/nixos/modules/home/gaming/default.nix
@@ -1,10 +1,20 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  inherit (lib) mkIf mkEnableOption;
+  cfg = config.my.home.gaming;
+in
 {
-  home.packages = with pkgs; [
-    goverlay
-    lutris
-    mangohud
-    vkbasalt
-  ];
+  options.my.home.gaming = {
+    enable = mkEnableOption "Gaming home options";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      goverlay
+      lutris
+      mangohud
+      vkbasalt
+    ];
+  };
 }
