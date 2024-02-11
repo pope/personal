749348a5024543b29524c5b55602b308b81e441a
K. Adam Christensen <pope@shifteleven.com>
2024-02-10 23:37:44 -0800

    Put virtualization into its own module

---
+++ etc/nixos/modules/nixos/virtualization/host.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkIf;
+  inherit (config.my.nixos) mainUser;
+  cfg = config.my.nixos.virtualization;
+in
+{
+  config = mkIf (cfg.enable && cfg.kind == "host") {
+    environment.systemPackages = with pkgs; [
+      qemu
+      virtiofsd
+    ];
+    programs.virt-manager.enable = true;
+    virtualisation.libvirtd.enable = true;
+    users.users."${mainUser}".extraGroups = [ "libvirtd" "kvm" ];
+  };
+}
