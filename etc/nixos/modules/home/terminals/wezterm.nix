fde8c7faa8374abab1ec56e90414c2eac034cf6b
K. Adam Christensen <pope@shifteleven.com>
2024-02-15 08:27:00 -0800

    Update WezTerm config for darwin.
    Also added an overrides to quickly test things.

--- etc/nixos/modules/home/terminals/wezterm.nix
+++ etc/nixos/modules/home/terminals/wezterm.nix
@@ -10,26 +10,40 @@ in
   };
 
   config = mkIf cfg.enable {
+    home.packages = [ pkgs.iosevka ];
+
     programs.wezterm = {
       enable = true;
-      extraConfig = ''
-        local wezterm = require 'wezterm'
+      extraConfig =
+        let
+          opacity = if pkgs.stdenv.isDarwin then 0.94 else 0.85;
+        in
+        ''
+          local config = wezterm.config_builder()
 
-        local config = wezterm.config_builder()
+          -- config.color_scheme = 'Rosé Pine (base16)'
+          -- config.color_scheme = 'Rosé Pine (Gogh)'
+          config.color_scheme = 'rose-pine'
+          config.default_cursor_style = "SteadyBar"
+          config.default_prog = { '${pkgs.fish}/bin/fish', '-l'}
+          config.enable_wayland = true
+          config.font = wezterm.font('Iosevka')
+          config.font_size = 11.5
+          config.hide_tab_bar_if_only_one_tab = true
+          config.initial_cols = 160
+          config.initial_rows = 30
+          config.line_height = 1.7
+          config.macos_window_background_blur = 20
+          config.use_resize_increments = true
+          config.window_background_opacity = ${builtins.toString opacity}
 
-        config.color_scheme = 'Rosé Pine (base16)'
-        --config.color_scheme = 'rose-pine'
-        config.default_prog = { '${pkgs.fish}/bin/fish', '-l'}
-        config.enable_wayland = true
-        config.font = wezterm.font('Iosevka')
-        config.font_size = 11.0
-        config.hide_tab_bar_if_only_one_tab = true
-        config.line_height = 1.25
-        config.use_resize_increments = true
-        config.window_background_opacity = 0.85
+          local overrides_exists, overrides = pcall(require, "overrides")
+          if overrides_exists then
+            overrides.config(config)
+          end
 
-        return config
-      '';
+          return config
+        '';
     };
   };
 }
