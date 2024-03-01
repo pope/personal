75b0bb055dca0699af00fdbdb5358bfb288fd527
K. Adam Christensen <pope@shifteleven.com>
2024-02-29 18:06:21 -0800

    Add nvim-surround
    And fix a typo.

--- etc/nvim/lua/plugins/editing.lua
+++ etc/nvim/lua/plugins/editing.lua
@@ -41,4 +41,10 @@ return {
 			},
 		},
 	},
+	{
+		'kylechui/nvim-surround',
+		version = '*', -- Use for stability; omit to use `main` branch for the latest features
+		event = { 'InsertEnter' },
+		config = true,
+	},
 }
