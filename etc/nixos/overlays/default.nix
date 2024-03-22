b239afd470dcb926c987679eb5dfdc93bfb07110
K. Adam Christensen <pope@shifteleven.com>
2024-03-22 14:29:08 -0700

    Remove waybar overlay.
    It doesn't seem like I need to patch anything anymore.

--- etc/nixos/overlays/default.nix
+++ etc/nixos/overlays/default.nix
@@ -15,12 +15,6 @@ in
       else /media/cyberia/Public/Software/rns_343_linux_arm64.tar.gz;
   };
 
-  waybar = prev.waybar.overrideAttrs (oldAttrs: {
-    mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-    postPatch = (oldAttrs.postPatch or "") + ''
-      sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp'';
-  });
-
 } // prev.lib.optionalAttrs prev.stdenv.isDarwin {
 
   ctpv = (prev.ctpv.override {
