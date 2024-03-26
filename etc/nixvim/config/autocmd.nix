d08106d4d19005570cdcce436c32fe8036dc9e7a
K. Adam Christensen <pope@shifteleven.com>
2024-03-26 07:25:52 -0700

    Fix formatting in autocmd.

--- etc/nixvim/config/autocmd.nix
+++ etc/nixvim/config/autocmd.nix
@@ -12,28 +12,28 @@ in
       group = "cursorColor";
       event = "ModeChanged";
       callback = lua ''
-                function()
-                  local auto = require("lualine.themes.auto")
-                  local modes = {
-                    ["n"] = auto.normal.a.bg,
-                    ["i"] = auto.insert.a.bg,
-                    ["c"] = auto.command.a.bg,
-                    ["V"] = auto.visual.a.bg,
-                    ["v"] = auto.visual.a.bg,
-                    [""] = auto.visual.a.bg,
-                    ["S"] = auto.visual.a.bg,
-                    ["s"] = auto.visual.a.bg,
-                    ["R"] = auto.replace.a.bg,
-                    ["r"] = auto.replace.a.bg,
-                    ["t"] = auto.command.a.bg,
-                  }
-                  local mode = vim.api.nvim_get_mode().mode
-                  local color = modes[mode] or modes.n
-        
-        					local base_highlight = vim.api.nvim_get_hl_by_name('CursorLineNr', true)
-                  local o = vim.tbl_extend("keep", { foreground = color }, base_highlight)
-                  vim.api.nvim_set_hl(0, "CursorLineNr", o)
-                end
+        function()
+          local auto = require("lualine.themes.auto")
+          local modes = {
+            ["n"] = auto.normal.a.bg,
+            ["i"] = auto.insert.a.bg,
+            ["c"] = auto.command.a.bg,
+            ["V"] = auto.visual.a.bg,
+            ["v"] = auto.visual.a.bg,
+            [""] = auto.visual.a.bg,
+            ["S"] = auto.visual.a.bg,
+            ["s"] = auto.visual.a.bg,
+            ["R"] = auto.replace.a.bg,
+            ["r"] = auto.replace.a.bg,
+            ["t"] = auto.command.a.bg,
+          }
+          local mode = vim.api.nvim_get_mode().mode
+          local color = modes[mode] or modes.n
+
+          local base_highlight = vim.api.nvim_get_hl_by_name('CursorLineNr', true)
+          local o = vim.tbl_extend("keep", { foreground = color }, base_highlight)
+          vim.api.nvim_set_hl(0, "CursorLineNr", o)
+        end
       '';
     }
     {
