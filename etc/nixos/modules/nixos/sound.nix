72507cea49eff9637f0e0cb1662de1608846990e
K. Adam Christensen <pope@shifteleven.com>
2024-04-20 09:03:34 -0700

    Add Jack and WirePlumber to sound config.
    Also just reduced some indenting.

--- etc/nixos/modules/nixos/sound.nix
+++ etc/nixos/modules/nixos/sound.nix
@@ -13,30 +13,19 @@ in
 
     sound.enable = true;
 
-    hardware = {
-      # Disable for pipewire
-      pulseaudio.enable = false;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      jack.enable = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
     };
 
-    services = {
+    # Disable for pipewire
+    hardware .pulseaudio.enable = false;
 
-      pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
-
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
-      };
-    };
-
-    security = {
-      # Enabled for pipewire
-      rtkit.enable = true;
-    };
+    # Enabled for pipewire
+    security.rtkit.enable = true;
   };
 }
