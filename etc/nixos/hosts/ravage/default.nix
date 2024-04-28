11773e48cb7f5a2706e2be9d67abf3c78ad97bad
K. Adam Christensen <pope@shifteleven.com>
2024-04-28 15:07:22 -0700

    Set TLP settings for Ravage.
    Trying out some settings to help improve battery life and battery health
    when the laptop is plugged in.

--- etc/nixos/hosts/ravage/default.nix
+++ etc/nixos/hosts/ravage/default.nix
@@ -79,7 +79,31 @@
   services = {
     # Power management
     thermald.enable = true;
-    tlp.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "balanced";
+
+        CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+
+        DISK_IDLE_SECS_ON_AC = 0;
+        DISK_IDLE_SECS_ON_BAT = 2;
+
+        START_CHARGE_THRESH_BAT0 = 85;
+        STOP_CHARGE_THRESH_BAT0 = 95;
+
+        START_CHARGE_THRESH_BAT1 = 85;
+        STOP_CHARGE_THRESH_BAT1 = 95;
+
+        MEM_SLEEP_ON_AC = "s2idle";
+        MEM_SLEEP_ON_BAT = "deep";
+      };
+    };
 
     openssh.enable = true;
   };
