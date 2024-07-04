49b6161304c0311f255d9e504df3c617215ba8a4
K. Adam Christensen <pope@shifteleven.com>
2024-07-04 09:08:29 -0700

    Add rumble
    Rumble is a Framework 13 laptop with an AMD chip.

---
+++ etc/nixos/hosts/rumble/default.nix
@@ -0,0 +1,78 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ ... }:
+
+{
+  imports =
+    [
+      ../../modules/nixos
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    initrd = {
+      luks.devices."luks-b3a5fbc2-0e7e-4b6a-9019-37629c91d744" = {
+        device = "/dev/disk/by-uuid/b3a5fbc2-0e7e-4b6a-9019-37629c91d744";
+      };
+    };
+
+    supportedFilesystems = [ "ntfs" ];
+  };
+
+  networking = {
+    hostName = "rumble";
+
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "America/Los_Angeles";
+
+  hardware.graphics.enable = true;
+
+  services.fwupd.enable = true;
+  # Must be explicitly false otherwise there's infinite recursion going on.
+  services.tlp.enable = false;
+
+  my.nixos = {
+    mainUser = "pope";
+
+    bluetooth.enable = true;
+    fonts.enable = true;
+    gaming.enable = true;
+    onepassword.enable = true;
+    sound.enable = true;
+    system.enable = true;
+    xserver = {
+      enable = true;
+
+      desktop = "gnome";
+
+      enableAutoLogin = false;
+      enableHyprland = true;
+    };
+  };
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
