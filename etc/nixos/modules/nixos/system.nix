99bee87349a332aed43cbc9946984527f18bd21b
K. Adam Christensen <pope@shifteleven.com>
2024-07-05 16:06:20 -0700

    Add script for setting up Flatpak

--- etc/nixos/modules/nixos/system.nix
+++ etc/nixos/modules/nixos/system.nix
@@ -40,6 +40,20 @@ in
       gparted
       ntfs3g
       wget
+
+      (pkgs.writeShellScriptBin "setup-flatpak" ''
+        ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+        ln -s /run/current-system/sw/share/X11/fonts $HOME/.local/share/fonts
+        ln -s /run/current-system/sw/share/themes $HOME/.themes
+        ${pkgs.flatpak}/bin/flatpak override --user \
+            --filesystem=xdg-config/gtk-3.0:ro \
+            --filesystem=xdg-config/gtkrc-2.0:ro \
+            --filesystem=xdg-config/gtk-4.0:ro \
+            --filesystem=xdg-config/gtkrc:ro \
+            --filesystem=$HOME/.themes:ro \
+            --filesystem=$HOME/.local/share/fonts:ro \
+            --filesystem=$HOME/.icons:ro
+      '')
     ];
 
     services = {
