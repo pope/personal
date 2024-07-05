1ed0aff5a2e24bb2d6c3bff0391acea6f1dae3d9
K. Adam Christensen <pope@shifteleven.com>
2024-07-05 08:43:08 -0700

    Use the hyprland tools instead of sway

--- etc/nixos/modules/home/hyprland/default.nix
+++ etc/nixos/modules/home/hyprland/default.nix
@@ -66,7 +66,6 @@ in
         pamixer
         slurp
         swappy
-        swayidle
         swww
         wdisplays # Tool for managing displays
         wf-recorder
@@ -75,9 +74,84 @@ in
       ];
     };
 
-    programs = {
-      wlogout.enable = true;
-      swaylock.enable = true;
+    programs.wlogout.enable = true;
+
+    programs.hyprlock = {
+      enable = true;
+      settings = {
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
+            blur_size = 3;
+            noise = 0.0117;
+            contrast = 0.8916;
+            brightness = 0.8172;
+            vibrancy = 0.1696;
+            vibrancy_darkness = 0.0;
+          }
+        ];
+        input-field = [
+          {
+            placeholder_text = "";
+          }
+        ];
+        label = [
+          {
+            text_align = "right";
+            halign = "center";
+            valign = "center";
+            text = "Hi there, $USER";
+            font_size = 50;
+            font_family = "Sans";
+            position = "0, 80";
+          }
+          {
+            text_align = "right";
+            halign = "center";
+            valign = "center";
+            text = "$TIME";
+            font_size = 150;
+            font_family = "Sans";
+            position = "0, 300";
+          }
+        ];
+      };
+    };
+
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+          before_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off && ${pkgs.systemd}/bin/loginctl lock-session";
+          ignore_dbus_inhibit = false;
+          lock_cmd = "${pkgs.procps}/bin/pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+        };
+        listener = [
+          {
+            timeout = 900;
+            on-timeout = "${pkgs.hyprlock}/bin/hyprlock";
+          }
+          {
+            timeout = 1200;
+            on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+            on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+          }
+        ];
+      };
+    };
+
+    services.hyprpaper = {
+      enable = true;
+      settings = {
+        ipc = "on";
+        splash = false;
+        splash_offset = 2.0;
+
+        preload = [ "~/Pictures/wallpaper-purple.png" ];
+        wallpaper = [ ",~/Pictures/wallpaper-purple.png" ];
+      };
     };
 
     xdg.configFile."swappy/config".text = ''
