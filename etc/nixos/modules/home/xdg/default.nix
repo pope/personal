9b11d43b5681fdc4559648b19262cca582d6a95f
K. Adam Christensen <pope@shifteleven.com>
2024-07-08 15:22:23 -0700

    Make Firefox the default HTML handler

--- etc/nixos/modules/home/xdg/default.nix
+++ etc/nixos/modules/home/xdg/default.nix
@@ -3,6 +3,23 @@
 let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.my.home.xdg;
+  mimetypes = {
+    "application/x-extension-htm" = "firefox.desktop";
+    "application/x-extension-html" = "firefox.desktop";
+    "application/x-extension-shtml" = "firefox.desktop";
+    "application/x-extension-xht" = "firefox.desktop";
+    "application/x-extension-xhtml" = "firefox.desktop";
+    "application/xhtml+xml" = "firefox.desktop";
+    "image/gif" = "imv.desktop";
+    "image/jpeg" = "imv.desktop";
+    "image/jpg" = "imv.desktop";
+    "image/png" = "imv.desktop";
+    "image/webp" = "imv.desktop";
+    "text/html" = "firefox.desktop";
+    "x-scheme-handler/chrome" = "firefox.desktop";
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+  };
 in
 {
   options.my.home.xdg = {
@@ -19,13 +36,8 @@ in
       cacheHome = config.home.homeDirectory + "/.cache";
 
       mimeApps.enable = true;
-      mimeApps.defaultApplications = {
-        "image/gif" = "imv.desktop";
-        "image/jpeg" = "imv.desktop";
-        "image/jpg" = "imv.desktop";
-        "image/png" = "imv.desktop";
-        "image/webp" = "imv.desktop";
-      };
+      mimeApps.associations.added = mimetypes;
+      mimeApps.defaultApplications = mimetypes;
 
       userDirs = {
         enable = true;
