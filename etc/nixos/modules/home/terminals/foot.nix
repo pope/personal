9270adbfe21f93088f9572ddc4708a6caa392181
K. Adam Christensen <pope@shifteleven.com>
2024-09-01 10:37:42 -0700

    Add theming to foot

--- etc/nixos/modules/home/terminals/foot.nix
+++ etc/nixos/modules/home/terminals/foot.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
 let
   inherit (lib) mkIf mkEnableOption;
@@ -12,12 +12,35 @@ in
   config = mkIf cfg.enable {
     programs.foot = {
       enable = true;
-      settings = {
+      settings = with config.my.home.theme.colors; {
         main = {
           dpi-aware = true;
           font = "Iosevka Comfy:size=10, Symbols Nerd Font:size=10";
-          # TODO(pope): Allow for other themes
-          include = "${lib.getOutput "themes" pkgs.foot}/share/foot/themes/rose-pine";
+        };
+        cursor = {
+          color = "${base00} ${base05}";
+        };
+        colors = {
+          background = "${base00}";
+          foreground = "${base05}";
+
+          regular0 = "${base02}"; # black
+          regular1 = "${base08}"; # red
+          regular2 = "${base0C}"; # green
+          regular3 = "${base09}"; # yellow
+          regular4 = "${base0B}"; # blue
+          regular5 = "${base0D}"; # magenta
+          regular6 = "${base0A}"; # cyan
+          regular7 = "${base05}"; # white
+
+          bright0 = "${base02}"; # bright black
+          bright1 = "${base08}"; # bright red
+          bright2 = "${base0C}"; # bright green
+          bright3 = "${base09}"; # bright yellow
+          bright4 = "${base0B}"; # bright blue
+          bright5 = "${base0D}"; # bright magenta
+          bright6 = "${base0A}"; # bright cyan
+          bright7 = "${base05}"; # bright white
         };
       };
     };
