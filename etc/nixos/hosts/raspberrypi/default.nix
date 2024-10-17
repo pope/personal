a5bddf0333fbb52d7b5a31f2c24a69d4fb6ce9dd
K. Adam Christensen <pope@shifteleven.com>
2024-10-16 18:39:51 -0700

    Add an rsync backup for Cyberia on the Pi
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/hosts/raspberrypi/default.nix
+++ etc/nixos/hosts/raspberrypi/default.nix
@@ -86,6 +86,38 @@
     };
   };
 
+  systemd = {
+    services.cyberia-backup = {
+      description = "Cyberia rsync daily backup service";
+      path = with pkgs; [ coreutils-full rsync ];
+      script = ''
+        LASTBACKUP=$(ls -d /mnt/Backup/Cyberia.*/ | sort | tail -1)
+        BACKUP="/mnt/Backup/Cyberia.$(date -d today +"%Y%m%d")"
+
+        if [ -d "$BACKUP" ]; then
+          echo "$BACKUP is already backed up. Skipping."
+        else
+          rsync -av --exclude=lost+found/ --link-dest="$LASTBACKUP" /mnt/Cyberia/ "$BACKUP"
+        fi
+      '';
+      after  = [ "mnt-Backup.mount" "mnt-Cyberia.mount" ];
+      wants  = [ "mnt-Backup.mount" "mnt-Cyberia.mount" ];
+      serviceConfig.Type = "oneshot";
+    };
+
+    timers.cyberia-backup = {
+      description = "Cyberia rsync daily backup timer";
+      wantedBy = [ "timers.target" ];
+      partOf = [ "cyberia-backup.service" ];
+      after  = [ "mnt-Backup.mount" "mnt-Cyberia.mount" ];
+      requires  = [ "mnt-Backup.mount" "mnt-Cyberia.mount" ];
+      timerConfig = {
+        OnBootSec = "5min";
+        OnUnitActiveSec = "6h";
+      };
+    };
+  };
+
   security.sudo.wheelNeedsPassword = false;
 
   time = {
