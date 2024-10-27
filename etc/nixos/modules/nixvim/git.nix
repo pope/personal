8ddeb4d70b4276bf5c49c91da1cf3b3e16c06bf7
K. Adam Christensen <pope@shifteleven.com>
2024-10-26 18:39:34 -0700

    Add lazygit nvim
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/nixvim/git.nix
+++ etc/nixos/modules/nixvim/git.nix
@@ -17,5 +17,20 @@ in
       event = [ "BufReadPre" "BufNewFile" ];
       config = true;
     }
+    {
+      pkg = lazygit-nvim;
+      lazy = true;
+      cmd = [
+        "LazyGit"
+        "LazyGitConfig"
+        "LazyGitCurrentFile"
+        "LazyGitFilter"
+        "LazyGitFilterCurrentFile"
+      ];
+      dependencies = [ plenary-nvim ];
+      keys = mkLazyKeys [
+        { lhs = "<leader>lg"; rhs = "<cmd>LazyGit<cr>"; desc = "Lazy Git"; }
+      ];
+    }
   ];
 }
