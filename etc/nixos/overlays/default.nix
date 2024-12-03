785414b1b043a0678d82211681b0e1e9c5675105
K. Adam Christensen <pope@shifteleven.com>
2024-12-02 19:41:17 -0800

    Add SF Mono font
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/overlays/default.nix
+++ etc/nixos/overlays/default.nix
@@ -7,7 +7,7 @@ in
 {
   inherit (mypkgs) fish-rose-pine fish-catppuccin fish-tokyonight;
   inherit (mypkgs) fsrcnnx modernx plow p5r-grub;
-  inherit (mypkgs) comic-code-ligatures lucida-grande monolisa;
+  inherit (mypkgs) comic-code-ligatures lucida-grande monolisa sf-mono-font;
   inherit (mypkgs) krigBilateral ssimDownscaler ssimSuperRes;
   inherit (mypkgs) iqm rbutil hatsune-miku-cursor yazi-plugins;
 
