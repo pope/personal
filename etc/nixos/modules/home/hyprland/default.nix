6cbbeacfc81c8c8110e6cec9811da23d619d289f
K. Adam Christensen <pope@shifteleven.com>
2024-12-16 16:02:19 -0800

    Update gamemode hyprland script
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/hyprland/default.nix
+++ etc/nixos/modules/home/hyprland/default.nix
@@ -10,12 +10,13 @@ let
       if [ "$HYPRGAMEMODE" = 1 ] ; then
           ${pkgs.hyprland}/bin/hyprctl --batch "\
               keyword animations:enabled 0;\
+              keyword decoration:rounding 0;\
               keyword decoration:drop_shadow 0;\
               keyword decoration:blur:enabled 0;\
+              keyword decoration:shadow:enabled 0;\
               keyword general:gaps_in 0;\
               keyword general:gaps_out 0;\
-              keyword general:border_size 1;\
-              keyword decoration:rounding 0" &> /dev/null
+              keyword general:border_size 1" &> /dev/null
           exit
       fi
       ${pkgs.hyprland}/bin/hyprctl reload
