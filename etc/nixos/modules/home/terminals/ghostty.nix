bd6c4f07c82e8142f6928aaa9c5420bb0de4b26a
K. Adam Christensen <pope@shifteleven.com>
2025-01-09 08:03:45 -0800

    Use Ghostty HM module
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/terminals/ghostty.nix
+++ etc/nixos/modules/home/terminals/ghostty.nix
@@ -4,12 +4,6 @@ let
   inherit (lib) mkIf mkEnableOption mkOption types;
   cfg = config.my.home.terminals.ghostty;
 
-  configDir =
-    if pkgs.stdenv.isDarwin then
-      "Library/Application Support/com.mitchellh.ghostty"
-    else
-      "${config.xdg.configHome}/ghostty";
-
   inherit (config.my.home.theme) colorScheme;
 in
 {
@@ -25,36 +19,34 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.packages = lib.optionals pkgs.stdenv.isLinux (with pkgs; [
-      ghostty
-    ]);
-
-    home.file."${configDir}/config".text =
-      let
-        theme =
-          if colorScheme == "rose-pine" then "rose-pine"
-          else if colorScheme == "catppuccin" then "catppuccin-mocha"
-          else if colorScheme == "dracula" then "Dracula"
-          else if colorScheme == "tokyonight" then "tokyonight"
-          else abort "invalid colorScheme";
-        window-decoration = if pkgs.stdenv.isLinux then "false" else "true";
-      in
-      ''
-        adjust-cell-height = "25%"
-
-        background-opacity = 0.95
-        background-blur-radius = 20
-
-        font-family = ""
-        font-family = "Liga SFMono Nerd Font"
-        font-family = "MonoLisa"
-        font-size = ${builtins.toString cfg.fontSize}
-
-        theme = "${theme}"
-
-        window-decoration = ${window-decoration}
-
-        config-file = ?overrides
-      '';
+    programs.ghostty = {
+      enable = true;
+      package = if pkgs.stdenv.isDarwin then null else pkgs.ghostty;
+      enableFishIntegration = config.my.home.shell.fish.enable;
+      enableZshIntegration = config.my.home.shell.zsh.enable;
+      settings =
+        let
+          theme =
+            if colorScheme == "rose-pine" then "rose-pine"
+            else if colorScheme == "catppuccin" then "catppuccin-mocha"
+            else if colorScheme == "dracula" then "Dracula"
+            else if colorScheme == "tokyonight" then "tokyonight"
+            else abort "invalid colorScheme";
+          window-decoration = if pkgs.stdenv.isLinux then "false" else "true";
+        in
+        {
+          inherit theme window-decoration;
+          adjust-cell-height = "25%";
+          background-opacity = 0.95;
+          background-blur-radius = 20;
+          config-file = [ "?overrides" ];
+          font-family = [
+            ""
+            "Liga SFMono Nerd Font"
+            "MonoLisa"
+          ];
+          font-size = cfg.fontSize;
+        };
+    };
   };
 }
