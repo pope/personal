6f9300baec9bd249c4a01913d489fd4f17429e31
K. Adam Christensen <pope@shifteleven.com>
2025-01-09 17:44:55 -0800

    Rename hardware.pulseaudio to services.pulseaudio
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/nixos/sound/default.nix
+++ etc/nixos/modules/nixos/sound/default.nix
@@ -10,17 +10,19 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      jack.enable = true;
-      pulse.enable = true;
-      wireplumber.enable = true;
-    };
+    services = {
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        jack.enable = true;
+        pulse.enable = true;
+        wireplumber.enable = true;
+      };
 
-    # Disable for pipewire
-    hardware.pulseaudio.enable = false;
+      # Disable for pipewire
+      pulseaudio.enable = false;
+    };
 
     # Enabled for pipewire
     security.rtkit.enable = true;
