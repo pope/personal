aeb1a3bd15073f7cec3d7d4186edd6a99ddf1e9e
K. Adam Christensen <pope@shifteleven.com>
2025-01-11 19:37:48 -0800

    Mount the NTFS drive on Unicron
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/hosts/unicron/default.nix
+++ etc/nixos/hosts/unicron/default.nix
@@ -66,6 +66,11 @@
     #   fsType = "ntfs-3g";
     #   options = [ "rw" "noauto,uid=1000,gid=100" "noatime" ];
     # };
+    "/media/games" = {
+       device = "/dev/nvme1n1p4";
+       fsType = "ntfs-3g";
+       options = [ "rw" "uid=pope" ];
+    };
     "/media/cyberia" = {
       device = "skrapnel.lan:/mnt/Cyberia";
       fsType = "nfs";
