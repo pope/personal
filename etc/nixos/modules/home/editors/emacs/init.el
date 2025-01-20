da2150c3ddbf1dc1d35b2b12368447ecd0bbdc7e
K. Adam Christensen <pope@shifteleven.com>
2025-01-20 11:41:47 -0800

    Use :custom for the doom-theme
    For consistency and whatnot.
    
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/editors/emacs/init.el
+++ etc/nixos/modules/home/editors/emacs/init.el
@@ -9,10 +9,10 @@
                   gc-cons-percentage 0.1)))
 
 (use-package doom-themes
+  :custom
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic t)
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-one t)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
