aa52a2b70bb4124169b5eb04ec6033b6134d4590
K. Adam Christensen <pope@shifteleven.com>
2025-02-27 16:14:17 -0800

    Add quicker nvim plugin
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/nixvim/ui/default.nix
+++ etc/nixos/modules/nixvim/ui/default.nix
@@ -22,6 +22,44 @@ in
       opts.update_focused_file.enable = true;
     }
     {
+      pkg = quicker-nvim;
+      event = [ "FileType qf" ];
+      keys = mkLazyKeys [
+        {
+          lhs = "<leader>q";
+          rhs = "<cmd>lua require('quicker').toggle()<CR>";
+          desc = "Toggle quickfix";
+        }
+        {
+          lhs = "<leader>l";
+          rhs = "<cmd>lua require('quicker').toggle({ loclist = true })<CR>";
+          desc = "Toggle loclist";
+        }
+        {
+          lhs = "<M-l>";
+          rhs = "<cmd>cnext<CR>";
+          desc = "Quickfix next";
+        }
+        {
+          lhs = "<M-h>";
+          rhs = "<cmd>cprev<CR>";
+          desc = "Quickfix previous";
+        }
+      ];
+      opts.keys = mkLazyKeys [
+        {
+          lhs = ">";
+          rhs = "<cmd>lua require('quicker').expand()<CR>";
+          desc = "Expand quicker content";
+        }
+        {
+          lhs = "<";
+          rhs = "<cmd>lua require('quicker').collapse()<CR>";
+          desc = "Collapse quicker content";
+        }
+      ];
+    }
+    {
       pkg = neoscroll-nvim;
       event = [ "VeryLazy" ];
       config = true;
