c9164e1efed8afd2a0375106834907fd4e5a0743
K. Adam Christensen <pope@shifteleven.com>
2025-05-23 14:35:53 -0700

    Create script for processing album art in my opus exports
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/multimedia/audio.nix
+++ etc/nixos/modules/home/multimedia/audio.nix
@@ -3,6 +3,55 @@
 let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.my.home.multimedia.audio;
+
+  an-album-cover = pkgs.writeShellApplication {
+    name = "an-album-cover";
+    runtimeInputs = with pkgs; [
+      findutils
+      imagemagick
+      opustags
+      parallel
+    ];
+    text = ''
+      function processDir() {
+        local cover="$1/cover.jpg"
+        local files
+
+        mapfile -t files < <(find "$1" -maxdepth 1 -name "*.opus")
+        if [[ -z "''${files[*]}" ]]; then
+          exit 0
+        fi
+
+        if [[ -f "$cover" ]]; then
+          >&2 echo "$1: cover art already found. Skipping"
+          exit 0
+        fi
+
+        # Load the album art of the first audio file. We can just re-use that
+        # for everything else
+        >&2 echo "$1: processing"
+        opustags --output-cover - "''${files[0]}" 2>/dev/null \
+          | magick - \
+            -resize 600x600 \
+            -adaptive-sharpen 0x0.5 \
+            -strip \
+            -interlace none \
+            "$cover" 2>/dev/null
+
+        if [[ ! -f "$cover" ]]; then
+          >&2 echo "$1: No cover art"
+          exit 1
+        fi
+
+        opustags --set-cover "$cover" -i "''${files[@]}"
+        >&2 echo "$1: finished"
+      }
+      export -f processDir
+
+      find ~/Music -type d -print0 | parallel -0 processDir
+    '';
+  };
+
   convert_48khz = pkgs.writeShellApplication {
     name = "convert_48khz";
     runtimeInputs = with pkgs; [
@@ -53,6 +102,7 @@ in
       convert_48khz
       convert_to_opus
     ] ++ lib.optionals stdenv.isLinux [
+      an-album-cover
       bitwig-studio
       easyaudiosync
       easytag
