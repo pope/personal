7ed42f53948f88b5b7f593ab92f0657ca3e9a108
K. Adam Christensen <pope@shifteleven.com>
2025-05-23 16:03:48 -0700

    Update rmpc config for lyrics
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/music/default.nix
+++ etc/nixos/modules/home/music/default.nix
@@ -18,7 +18,55 @@ in
         #![enable(unwrap_variant_newtypes)]
         (
           address: "/run/user/1000/mpd/socket",
+          artists: (
+            album_display_mode: NameOnly,
+            album_sort_by: Date,
+          ),
           cache_dir: Some("${config.xdg.dataHome}/rmpc"),
+          enable_mouse: true,
+          lyrics_dir: Some("${config.xdg.userDirs.music}"),
+          tabs: [
+            (
+              name: "Queue",
+              pane: Split(
+                direction: Horizontal,
+                panes: [
+                  (size: "60%", pane: Pane(Queue)),
+                  (size: "40%", pane: Split(
+                    direction: Vertical,
+                    panes: [
+                      (size: "70%", pane: Pane(AlbumArt)),
+                      (size: "30%", pane: Pane(Lyrics)),
+                    ],
+                  )),
+                ],
+              ),
+            ),
+            (
+              name: "Directories",
+              pane: Pane(Directories),
+            ),
+            (
+              name: "Artists",
+              pane: Pane(Artists),
+            ),
+            (
+              name: "Album Artists",
+              pane: Pane(AlbumArtists),
+            ),
+            (
+              name: "Albums",
+              pane: Pane(Albums),
+            ),
+            (
+              name: "Playlists",
+              pane: Pane(Playlists),
+            ),
+            (
+              name: "Search",
+              pane: Pane(Search),
+            ),
+          ],
         )
       '';
     };
