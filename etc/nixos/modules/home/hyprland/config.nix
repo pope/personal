e9f430e33338f2a673d06668640ae5729e628cac
K. Adam Christensen <pope@shifteleven.com>
2025-05-26 07:45:22 -0700

    Fix typo on lib.optionals
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/hyprland/config.nix
+++ etc/nixos/modules/home/hyprland/config.nix
@@ -113,13 +113,13 @@ in
 
       animations = {
         enabled = !cfg.enableBatterySaverMode;
-        bezier = lib.optional (!cfg.enableBatterySaverMode) [
+        bezier = lib.optionals (!cfg.enableBatterySaverMode) [
           "wind, 0.05, 0.9, 0.1, 1.05"
           "winIn, 0.1, 1.1, 0.1, 1.1"
           "winOut, 0.3, -0.3, 0, 1"
           "liner, 1, 1, 1, 1"
         ];
-        animation = lib.optional (!cfg.enableBatterySaverMode) [
+        animation = lib.optionals (!cfg.enableBatterySaverMode) [
           "windows, 1, 6, wind, slide"
           "windowsIn, 1, 6, winIn, slide"
           "windowsOut, 1, 5, winOut, slide"
