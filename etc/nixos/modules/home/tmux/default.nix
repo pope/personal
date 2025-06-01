04ac55b82bedad10e7a0a43b344f0eb0248e0794
K. Adam Christensen <pope@shifteleven.com>
2025-06-01 10:11:21 -0700

    Opt for a simpler tmux UI config
    Dropping theme support (which makes per-client themes over SSH behave
    better) and don't add a lot of context. That other data is usually
    accessible in other places, so fewer distractions are just better
    
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/tmux/default.nix
+++ etc/nixos/modules/home/tmux/default.nix
@@ -2,7 +2,6 @@
 
 let
   cfg = config.my.home.tmux;
-  inherit (config.my.home.theme) colorScheme;
 in
 {
   options.my.home.tmux = {
@@ -35,58 +34,9 @@ in
         { plugin = tmux-fzf; }
         { plugin = yank; }
         { plugin = tmux-thumbs; extraConfig = "set -g @thumbs-osc52 1"; }
-      ] ++ lib.optionals (colorScheme == "rose-pine") [
-        {
-          plugin = rose-pine;
-          extraConfig = ''
-            set -g @rose_pine_variant 'main'
-            set -g @rose_pine_date_time '%Y-%m-%d %H:%M'
-            set -g @rose_pine_disable_active_window_menu 'on'
-            set -g @rose_pine_bar_bg_disable 'on'
-            set -g @rose_pine_bar_bg_disabled_color_option 'default'
-          '';
-        }
-      ] ++ lib.optionals (colorScheme == "catppuccin") [
-        {
-          plugin = catppuccin;
-          extraConfig = ''
-            set -g @catppuccin_window_left_separator ""
-            set -g @catppuccin_window_right_separator " "
-            set -g @catppuccin_window_middle_separator " █"
-            set -g @catppuccin_window_number_position "right"
-
-            set -g @catppuccin_window_default_fill "number"
-            set -g @catppuccin_window_default_text "#W"
-
-            set -g @catppuccin_window_current_fill "number"
-            set -g @catppuccin_window_current_text "#W"
-
-            set -g @catppuccin_status_modules_right "directory user host session"
-            set -g @catppuccin_status_left_separator  " "
-            set -g @catppuccin_status_right_separator ""
-            set -g @catppuccin_status_right_separator_inverse "no"
-            set -g @catppuccin_status_fill "icon"
-            set -g @catppuccin_status_connect_separator "no"
-
-            set -g @catppuccin_directory_text "#{pane_current_path}"
-          '';
-        }
-      ] ++ lib.optionals (colorScheme == "dracula") [
-        {
-          plugin = dracula;
-          extraConfig = ''
-            set -g @dracula-show-powerline true
-            set -g @dracula-show-left-sep ""
-            set -g @dracula-show-right-sep ""
-          '';
-        }
-      ] ++ (with pkgs.tmuxPlugins; [
-        # These come last because continuum updates the `status-left` &
-        # `status-right` variables - and thus need to come after the themeing
-        # stuff above.
         { plugin = resurrect; extraConfig = "set -g @resurrect-strategy-nvim 'session'"; }
         { plugin = continuum; extraConfig = "set -g @continuum-restore 'on'"; }
-      ]);
+      ];
       extraConfig = ''
         set-option -sa terminal-features ',alacritty*:RGB,foot*:RGB,xterm-*:RGB'
 
@@ -97,6 +47,23 @@ in
         set -g update-environment -r
         set -g renumber-windows on
         set -g status-position top
+
+        # Theme: borders
+        set -g pane-border-indicators both
+        set -g pane-border-lines heavy
+        set -g pane-border-style fg=black,bright
+        set -g pane-active-border-style fg=magenta
+
+        # Theme: status
+        set -g status-style bg=default,fg=white
+        set -g status-left ""
+        set -g status-right "#[fg=white,bright]#S"
+
+        # Theme: status (windows)
+        set -g window-status-format " ○ #W "
+        set -g window-status-current-format " ● #W "
+        set -g window-status-current-style "#{?window_zoomed_flag,fg=yellow,fg=magenta,nobold}"
+        set -g window-status-bell-style "fg=red,nobold"
 
         bind '"' split-window -c "#{pane_current_path}"
         bind % split-window -h -c "#{pane_current_path}"
