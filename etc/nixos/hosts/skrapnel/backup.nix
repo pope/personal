d07e955218fb7278960da2f5e74fab8a7b2cba0a
K. Adam Christensen <pope@shifteleven.com>
2025-07-03 09:47:41 -0700

    Update backup script to cleanup up old dirs
    Now keeps the last 7 days, plus the 1st and 15th of every month
    
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/hosts/skrapnel/backup.nix
+++ etc/nixos/hosts/skrapnel/backup.nix
@@ -17,10 +17,22 @@
         LASTBACKUP=$(find /mnt/Backup/ -maxdepth 1 -iname "Cyberia.*" -type d | sort | tail -1)
         BACKUP="/mnt/Backup/Cyberia.$(date -d today +"%Y%m%d")"
 
+        TO_CLEANUP="/mnt/Backup/Cyberia.$(date -d "7 days ago" +"%Y%m%d")"
+
         if [ -d "$BACKUP" ]; then
           echo "$BACKUP is already backed up. Skipping."
         else
+          echo "Backing up $BACKUP from $LASTBACKUP"
           rsync -av --exclude=lost+found/ --link-dest="$LASTBACKUP" /mnt/Cyberia/ "$BACKUP"
+          # If the sync succeeds, then clean up the backup 7 days ago, unless
+          # that backup was from the 1st or 15th.
+          if [ $? -eq 0 ] && [[ ! "$TO_CLEANUP" =~ (01|15)$ ]]; then
+            echo "Removing $TO_CLEANUP from 7 days ago"
+            rm -rf "$TO_CLEANUP"
+          else
+            echo "Keeping $TO_CLEANUP from 7 days ago"
+          fi
+          echo "$BACKUP done."
         fi
       '';
       after = [ "mnt-Backup.mount" "mnt-Cyberia.mount" ];
