960c8e26cf9ece7f7791a3b9381ea5253e622cd7
K. Adam Christensen <pope@shifteleven.com>
2025-07-22 09:59:09 -0700

    Update nixpkgs and de-grade Darktable
    There are issues with building darktable at the moment, so downgrading
    to the stable version.
    
    With the downgrade, also trying out the mnative compile flags to see if
    that helps performance
    
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/multimedia/photography.nix
+++ etc/nixos/modules/home/multimedia/photography.nix
@@ -10,7 +10,12 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
-      darktable
+      # See https://github.com/NixOS/nixpkgs/pull/426650
+      # So defaulting back to the stable version
+      (stable.darktable.override {
+        # While we are at it, why not try using native optimizations?
+        stdenv = impureUseNativeOptimizations stdenv;
+      })
       dnglab
       geeqie
       rawtherapee
