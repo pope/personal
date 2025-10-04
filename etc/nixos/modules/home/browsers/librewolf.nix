b8cc8a721578ff5658c315bf87d3419da3713d0f
K. Adam Christensen <pope@shifteleven.com>
2025-10-03 17:47:09 -0700

    Remove firefox extensions from librewolf
    The 1Password extension was failing. Installing from the Firefox Addons
    site made it reliable again
    
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/browsers/librewolf.nix
+++ etc/nixos/modules/home/browsers/librewolf.nix
@@ -1,8 +1,7 @@
-{ inputs, pkgs, config, lib, ... }:
+{ config, lib, ... }:
 
 let
   cfg = config.my.home.browsers.librewolf;
-  ffaddons = inputs.firefox-addons.packages."${pkgs.system}";
 in
 {
   options.my.home.browsers.librewolf = {
@@ -14,11 +13,6 @@ in
       enable = true;
       profiles.default = {
         isDefault = true;
-        extensions.packages = [
-          # TODO(pope): Find a better way to fix this hack.
-          # Shout out to https://sourcegraph.com/github.com/niksingh710/ndots@b157c0d7337c37d098f3033343f4ed0822575be9/-/blob/modules/home/browsers/firefox/default.nix?L52-57
-          (ffaddons."1password-x-password-manager".overrideAttrs { meta.license = lib.licenses.free; })
-        ];
       };
     };
   };
