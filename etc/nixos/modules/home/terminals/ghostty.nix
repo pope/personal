1b787bceaea8bc60bb5610eaf20efaad393dadc1
K. Adam Christensen <pope@shifteleven.com>
2025-10-19 10:46:25 -0700

    Use pkgs.ghostty-bin for MacOS
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/terminals/ghostty.nix
+++ etc/nixos/modules/home/terminals/ghostty.nix
@@ -25,7 +25,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.ghostty = {
       enable = true;
-      package = if pkgs.stdenv.isDarwin then pkgs.bashInteractive else config.lib.nixGL.wrap pkgs.ghostty;
+      package = if pkgs.stdenv.isDarwin then pkgs.ghostty-bin else config.lib.nixGL.wrap pkgs.ghostty;
       enableFishIntegration = config.my.home.shell.fish.enable;
       enableZshIntegration = config.my.home.shell.zsh.enable;
       installBatSyntax = !pkgs.stdenv.isDarwin;
