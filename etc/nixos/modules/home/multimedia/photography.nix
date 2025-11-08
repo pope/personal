c02b645208ec5697782ce430e01478798c51a26e
K. Adam Christensen <pope@shifteleven.com>
2025-11-07 18:07:10 -0800

    Clean up digikam config
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- etc/nixos/modules/home/multimedia/photography.nix
+++ etc/nixos/modules/home/multimedia/photography.nix
@@ -16,17 +16,7 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       darktable
-      # TODO(pope): Clean up once https://github.com/NixOS/nixpkgs/pull/453725 is avail
-      (digikam.overrideAttrs (_oldAttrs: {
-        version = "8.8.0";
-        src = fetchFromGitLab {
-          domain = "invent.kde.org";
-          owner = "graphics";
-          repo = "digikam";
-          tag = "v8.8.0";
-          hash = "sha256-yUrB0FXUcm+6QtlB7HMqdPpdhrV2iAo1oRkjgsHJiCU=";
-        };
-      }))
+      digikam
       dnglab
       geeqie
       rawtherapee
