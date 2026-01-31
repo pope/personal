d62a68b4f2520800d3864b00d23329e684180f28
K. Adam Christensen <pope@shifteleven.com>
2026-01-30 17:11:44 -0800

    Add Lofree Flow keyboard support
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- modules/nixos/keyboards/default.nix
+++ modules/nixos/keyboards/default.nix
@@ -14,6 +14,11 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    # https://codeberg.org/alexeygumirov/lofree-flow-fn-fix
+    boot.extraModprobeConfig = ''
+      options hid_apple fnmode=2 swap_opt_cmd=1
+    '';
+
     environment.systemPackages = with pkgs; [
       keymapp
       qmk
