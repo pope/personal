d3cd9278f9d11f8453dccb8f503b0b22876f2d57
K. Adam Christensen <pope@shifteleven.com>
2026-02-19 20:35:13 -0800

    Update flake and address broken packages
    - NDI released a new version and update script hadn't run yet
    - Open vSwitch update has flaky tests, ping to unstable
    
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- modules/home/obs/default.nix
+++ modules/home/obs/default.nix
@@ -17,7 +17,15 @@ in
     programs.obs-studio = {
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [
-        distroav
+        # TODO(pope): Remove this override after the NDI updater script runs
+        (distroav.override {
+          ndi-6 = pkgs.ndi-6.overrideAttrs (_: {
+            src = pkgs.fetchurl {
+              url = "https://downloads.ndi.tv/SDK/NDI_SDK_Linux/Install_NDI_SDK_v6_Linux.tar.gz";
+              hash = "sha256-wLXfFzJIiGJ7ZSF8e4UNdQKHxS4z6WSF4qprESKeYD4=";
+            };
+          });
+        })
         input-overlay
         obs-pipewire-audio-capture
         obs-vaapi
