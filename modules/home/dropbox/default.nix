bd849181b7dec8a637ec0f9a4d922bc823e2749e
K. Adam Christensen <pope@shifteleven.com>
2026-04-22 19:43:37 -0700

    Add the maestral config
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- modules/home/dropbox/default.nix
+++ modules/home/dropbox/default.nix
@@ -11,8 +11,6 @@ in
 {
   options.my.home.dropbox = {
     enable = lib.mkEnableOption "Dropbox support home options";
-    # TODO(pope): Copy over the config so that the app doesn't need to be
-    # set up before the service can run.
     service.enable = lib.mkEnableOption "Enable Dropbox support service";
     gui.enable = lib.mkEnableOption "Enable Dropbox support GUI";
   };
@@ -29,6 +27,40 @@ in
           maestral-gui
         ]
       );
+
+    # The Config file
+    sops.secrets.maestral-account-id = { };
+    sops.templates."maestral.ini".content = lib.generators.toINI { } {
+      auth = {
+        account_id = config.sops.placeholder.maestral-account-id;
+        keyring = "keyrings.alt.file.PlaintextKeyring";
+        token_access_type = "offline";
+      };
+      app = {
+        notification_level = 15;
+        log_level = 20;
+        update_notification_interval = 604800;
+        bandwidth_limit_up = 0.0;
+        bandwidth_limit_down = 0.0;
+        max_parallel_uploads = 6;
+        max_parallel_downloads = 6;
+      };
+      sync = {
+        path = "/mnt/Cyberia/Dropbox";
+        # TODO(pope): Support lists
+        excluded_items = "[]";
+        max_cpu_percent = 20.0;
+        keep_history = 604800;
+        upload = true;
+        download = true;
+      };
+      main = {
+        version = "20.0";
+      };
+    };
+    xdg.configFile."maestral/maestral.ini".source =
+      config.lib.file.mkOutOfStoreSymlink
+        config.sops.templates."maestral.ini".path;
 
     systemd.user.services = lib.mkIf cfg.service.enable {
       maestral = {
