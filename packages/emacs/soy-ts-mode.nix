567b9979191ae920618fa011453e857aea8e1776
K. Adam Christensen <pope@shifteleven.com>
2026-06-29 10:06:21 -0700

    Fixed the update script to the soy treesitter stuff
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- packages/emacs/soy-ts-mode.nix
+++ packages/emacs/soy-ts-mode.nix
@@ -1,27 +1,57 @@
 {
+  coreutils,
   fetchgit,
+  gnused,
+  jq,
+  lib,
   melpaBuild,
-  nix-update-script,
+  nix-prefetch-git,
+  writeShellApplication,
 }:
 
 melpaBuild {
   pname = "soy-ts-mode";
-  version = "0-unstable-2026-06-28";
+  version = "0-unstable-2026-06-29";
 
   src = fetchgit {
     url = "https://p0.pe/my-git-repos/tree-sitter-soy.git";
-    rev = "7d637ad03371e9c66ddaad47453076da16bf3b25";
-    hash = "sha256-gNUTUZSV7TqTdmNqUcwZPefnI+QDYXxb2+XnJHxNHRM=";
+    rev = "1f38f60f4288f9ce69549ead731bbbf798d9537d";
+    hash = "sha256-Kw148XfmhE5+kHpCKxQhhJZpwkmSF1Lb1U175tQQ7q8=";
   };
 
   recipe = ''
     (soy-ts-mode :fetcher git :url "" :files ("emacs/soy-ts-mode.el"))
   '';
 
-  passthru.updateScript = nix-update-script {
-    extraArgs = [
-      "--flake"
-      "--version=branch"
-    ];
-  };
+  passthru.updateScript =
+    let
+      updateScript = writeShellApplication {
+        name = "update-soy-ts-mode";
+        runtimeInputs = [
+          nix-prefetch-git
+          jq
+          gnused
+          coreutils
+        ];
+        text = ''
+          file="packages/emacs/soy-ts-mode.nix"
+          url=$(sed -n -E 's/.*url = "([^"]+)";.*/\1/p' "$file" | head -n1)
+
+          metadata=$(nix-prefetch-git "$url")
+
+          rev=$(echo "$metadata" | jq -r '.rev')
+          hash=$(echo "$metadata" | jq -r '.hash')
+          date=$(echo "$metadata" | jq -r '.date' | cut -d'T' -f1)
+
+          version="0-unstable-$date"
+
+          sed -i -E "s/version = \"[^\"]+\";/version = \"$version\";/" "$file"
+          sed -i -E "s/rev = \"[^\"]+\";/rev = \"$rev\";/" "$file"
+          sed -i -E "s/hash = \"[^\"]+\";/hash = \"$hash\";/" "$file"
+
+          echo "Updated soy-ts-mode to version $version, rev $rev, hash $hash"
+        '';
+      };
+    in
+    [ (lib.getExe updateScript) ];
 }
--- packages/tree-sitter-soy.nix
+++ packages/tree-sitter-soy.nix
@@ -1,26 +1,55 @@
 {
-  lib,
+  coreutils,
   fetchgit,
-  nix-update-script,
+  gnused,
+  jq,
+  lib,
+  nix-prefetch-git,
   tree-sitter,
+  writeShellApplication,
 }:
 
 tree-sitter.buildGrammar {
   language = "soy";
-  version = "0-unstable-2026-06-28";
+  version = "0-unstable-2026-06-29";
 
   src = fetchgit {
     url = "https://p0.pe/my-git-repos/tree-sitter-soy.git";
-    rev = "7d637ad03371e9c66ddaad47453076da16bf3b25";
-    hash = "sha256-gNUTUZSV7TqTdmNqUcwZPefnI+QDYXxb2+XnJHxNHRM=";
+    rev = "1f38f60f4288f9ce69549ead731bbbf798d9537d";
+    hash = "sha256-Kw148XfmhE5+kHpCKxQhhJZpwkmSF1Lb1U175tQQ7q8=";
   };
 
-  passthru.updateScript = nix-update-script {
-    extraArgs = [
-      "--flake"
-      "--version=branch"
-    ];
-  };
+  passthru.updateScript =
+    let
+      updateScript = writeShellApplication {
+        name = "update-tree-sitter-soy";
+        runtimeInputs = [
+          nix-prefetch-git
+          jq
+          gnused
+          coreutils
+        ];
+        text = ''
+          file="packages/tree-sitter-soy.nix"
+          url=$(sed -n -E 's/.*url = "([^"]+)";.*/\1/p' "$file" | head -n1)
+
+          metadata=$(nix-prefetch-git "$url")
+
+          rev=$(echo "$metadata" | jq -r '.rev')
+          hash=$(echo "$metadata" | jq -r '.hash')
+          date=$(echo "$metadata" | jq -r '.date' | cut -d'T' -f1)
+
+          version="0-unstable-$date"
+
+          sed -i -E "s/version = \"[^\"]+\";/version = \"$version\";/" "$file"
+          sed -i -E "s/rev = \"[^\"]+\";/rev = \"$rev\";/" "$file"
+          sed -i -E "s/hash = \"[^\"]+\";/hash = \"$hash\";/" "$file"
+
+          echo "Updated tree-sitter-soy to version $version, rev $rev, hash $hash"
+        '';
+      };
+    in
+    [ (lib.getExe updateScript) ];
 
   meta = {
     homepage = "https://p0.pe/my-git-repos/tree-sitter-soy.git";
