6926f1ce914ef8b2617c1c137b1d8c406aeb3971
K. Adam Christensen <pope@shifteleven.com>
2026-07-01 12:09:47 -0700

    Add some KDE theme thingies
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- modules/home/kde/default.nix
+++ modules/home/kde/default.nix
@@ -1,7 +1,25 @@
-{ lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
+let
+  cfg = config.my.home.kde;
+in
 {
   options.my.home.kde = {
     enable = lib.mkEnableOption "KDE home options";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      kdePackages.aurorae
+      kdePackages.oxygen
+      kdePackages.oxygen-icons
+      kdePackages.oxygen-sounds
+      oxygenfonts
+    ];
   };
 }
